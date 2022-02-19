b47925a10c328fb6d36a762edc0954242f50b26b
Nathan McCutchen <n8wm@icloud.com>
2022-02-19 15:12:58 -0800

    Movable objects now indicate where they will be dropped with an arrow

--- Assets/Scripts/MovableObjectRespawn.cs
+++ Assets/Scripts/MovableObjectRespawn.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class MovableObjectRespawn : MonoBehaviour
 {
     [SerializeField] float respawnY;
+    [SerializeField] float indicatorDistance = 5;
+    [SerializeField] LayerMask colliderMask;
     Vector3 startPosition;
+    float indCurLen = 0;
+    float indSpeed = 20f;
+    LineRenderer lr;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.position;
+        lr = GetComponent<LineRenderer>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,9 +27,33 @@ public class MovableObjectRespawn : MonoBehaviour
         if (transform.position.y < respawnY)
         {
             transform.position = startPosition;
-            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            rb.velocity = Vector3.zero;
         }
     }
+
+    private void LateUpdate()
+    {
+        if (rb.isKinematic)
+        {
+            lr.positionCount = 2;
+            lr.SetPosition(0, transform.position);
+
+            Ray dir = new Ray(transform.position, Vector3.down * indicatorDistance);
+
+            if (Physics.Raycast(dir, out RaycastHit hit, indicatorDistance, colliderMask))
+                indCurLen = Mathf.Lerp(indCurLen, transform.position.y - hit.point.y, Time.deltaTime * indSpeed);
+            else
+                indCurLen = Mathf.Lerp(indCurLen, indicatorDistance, Time.deltaTime * indSpeed);
+
+            lr.SetPosition(1, transform.position - new Vector3(0, indCurLen - 0.3f, 0));
+        }
+        else
+        {
+            lr.positionCount = 0;
+            indCurLen = 0;
+        }
+    }
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, respawnY, transform.position.z));
