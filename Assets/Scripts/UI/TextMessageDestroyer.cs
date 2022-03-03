d1d453405ae94aadfb962f5e9b633bb0bff6f6a4
Runchimchar <Jpwolf101@gmail.com>
2022-03-03 14:45:23 -0800

    Added fade in animation to messages

--- Assets/Scripts/UI/TextMessageDestroyer.cs
+++ Assets/Scripts/UI/TextMessageDestroyer.cs
@@ -15,21 +15,40 @@ public class TextMessageDestroyer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Start fading the message
-        Invoke("FadeMessage", fadeDelay);
+        // Fade in message
+        canvasGroup.alpha = 0f;
+        FadeInMessage();
+
+        // Prepare to fade out message
+        Invoke("FadeOutMessage", fadeDelay + fadeTime);
     }
 
-    private void FadeMessage() {
+    private void FadeInMessage() {
+        // Increase alpha
+        canvasGroup.alpha = Mathf.Min(canvasGroup.alpha + fadeStep, 1f);
+        //Debug.Log("Alpha is " + canvasGroup.alpha);
+        // Check if alpha is 0
+        if (canvasGroup.alpha < 0.999f) {
+            Invoke("FadeInMessage", fadeTime * fadeStep);
+        }
+    }
+
+    private void FadeOutMessage() {
         // Reduce alpha
         canvasGroup.alpha = Mathf.Max(canvasGroup.alpha - fadeStep, 0f);
-        Debug.Log("Alpha is " + canvasGroup.alpha);
+        //Debug.Log("Alpha is " + canvasGroup.alpha);
         // Check if alpha is 0
         if (canvasGroup.alpha <= 0.001f) {
-            Destroy(gameObject);
-            DestroyEvent();
-        } else {
-            Debug.Log("Invoking in " + (fadeTime * fadeStep) + " seconds");
-            Invoke("FadeMessage", fadeTime * fadeStep);
+            Destroy(gameObject, 0.5f);
         }
+        else {
+            //Debug.Log("Invoking in " + (fadeTime * fadeStep) + " seconds");
+            Invoke("FadeOutMessage", fadeTime * fadeStep);
+        }
+    }
+
+    // Notify that message is being destroyed
+    private void OnDestroy() {
+        DestroyEvent();
     }
 }
